e10630be583f5641f04211bf330fa184b7b82bec
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-11-22 22:03:39 +0900

    comment: 불필요한 메서드 제거

--- src/main/java/jongseol/inha_helper/repository/MemberAssignmentRepository.java
+++ src/main/java/jongseol/inha_helper/repository/MemberAssignmentRepository.java
@@ -9,15 +9,11 @@ import java.util.List;
 @Repository
 public interface MemberAssignmentRepository extends JpaRepository<MemberAssignment, Long> {
 
-    boolean existsMemberAssignmentByAssignment_WebId(Long webId);
-
-
     boolean existsMemberAssignmentByAssignment_IdAndMember_Id(Long webId, Long memberId);
 
     List<MemberAssignment> findMemberAssignmentsByCompletedAndMember_Id(boolean completed, Long memberId);
 
     List<MemberAssignment> findMemberAssignmentsByMember_Id(Long memberId);
-    MemberAssignment findMemberAssignmentByAssignment_IdAndMember_Id(Long assignId, Long memberId);
 
     List<MemberAssignment> findMemberAssignmentsByCompleted(boolean completed);
 
